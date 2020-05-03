d7e2c92b28c83242132bd61dc041ffd688a32d91
Beware <mis.zolotarew@gmail.com>
2020-05-03 14:04:53 +0300

    task3 completed

---
+++ lesson-git/hometask4.js
@@ -0,0 +1,9 @@
+function spiral(n) {
+    let str = ''
+    for (let index = 0; index < n; index++) {
+        str = "1" * n + "\n";
+        
+    }
+    return str;
+}
+console.log(spiral(6))
--- lesson-git/task3.js
+++ lesson-git/task3.js
@@ -1,16 +1,18 @@
 function task3(pairs) {
-    massiv = pairs
+    massiv = pairs;
     for (let para of massiv) {
         if (para[0] === 0 || para[1] === 0) {
-            let val = para[0]
-            para[0] = para[1]
-            para[1] = val
+            let val = para[0];
+            para[0] = para[1];
+            para[1] = val;
         }
         if (para[0] % 2 != 0) {
-            para[0] = para[0] * 2
+            para[0] = para[0] * 2;
+        }
+        if (para[1] % 2 != 0) {
+            para[1] = para[1] * 2;
         }
     }
-    return massiv
-
+    return massiv;
 }
-console.log(task3([[1,2],[3,6],[0,5]]))
+console.log(task3([[1,2],[3,7],[0,5]]))
