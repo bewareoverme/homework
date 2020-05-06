ea58e8cd08891a3ff23a0d43aec7f6f0af32c193
Beware <mis.zolotarew@gmail.com>
2020-05-06 10:58:31 +0300

    spiral

--- lesson-git/hometask4.js
+++ lesson-git/hometask4.js
@@ -1,9 +1,37 @@
 function spiral(n) {
-    let str = ''
-    for (let index = 0; index < n; index++) {
-        str = "1" * n + "\n";
-        
+    let result = new Array(n).fill().map(() => new Array(n).fill(""));
+    let counter = 1;
+    let startRaw = 0;
+    let endRaw = n - 1;
+    let startCol = 0;
+    let endCol = n - 1;
+    
+
+    while (startRaw <= endRaw && startCol <= endCol) {
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRaw][i] = counter;
+            counter++;
+        }
+        startRaw++
+
+        for (let j = startRaw; j <= endRaw; j++) {
+            result[j][endCol] = counter;
+            counter++
+        }
+        endCol--
+
+        for (let i = endCol; i >= startCol; i--) {
+            result[endRaw][i] = counter;
+            counter++
+        }
+        endRaw--
+
+        for (let i = endRaw; i >= startRaw; i--) {
+            result[i][startCol] = counter;
+            counter++
+        }
+        startCol++;
     }
-    return str;
+    return result;
 }
 console.log(spiral(6))
